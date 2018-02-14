179d37f6bf3ea11ed16ae9cf19f938643ac15a60
Tapan Soni <32372516+TapanSoni@users.noreply.github.com>
2018-02-14 11:26:59 -0500

    This is my learn.py file

---
+++ Learn.py
@@ -0,0 +1,22 @@
+#
+# Example file for HelloWorld
+#
+# Define the main function
+def main():
+    print("Hello World")
+
+
+# Call the main function so it prints out "Hello World"
+main()
+
+# VARIABLES IN PYTHON
+
+# Declare a variable and initialize it
+f = 0
+print(f)
+
+# Re-declaring the variable works -- types can be changed
+f = "abc"
+print(f)
+
+# ERROR: Variables of different types cannot be combined.
