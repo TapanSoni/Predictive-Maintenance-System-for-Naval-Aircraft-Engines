a140f373fcc4f224f512ad81031b58e3eb85ee0c
Michael Matthews <ipodappsrcool@gmail.com>
2018-02-24 17:10:30 -0500

    Importing csv into python.

--- Predictive-Maintenance-System-master/importCSV.py
+++ Predictive-Maintenance-System-master/importCSV.py
@@ -1,2 +1,8 @@
-import pandas as pd
-pd.read_csv('/Users/MM/Downloads/data.csv') #File path for file you'd like to import
+#Written by Michael Matthews
+#Reads in dataset as an array.
+#Must have numpy installed to run
+
+import numpy as np
+
+whole_data_set = np.genfromtxt('/Users/MM/Downloads/data.txt', delimiter='\t') #File path for file you'd like to import
+
