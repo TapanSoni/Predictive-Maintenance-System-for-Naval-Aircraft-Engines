d52bfea030a19d07d5ff72e059e64f9bbac081c3
Nic <ncklasala@gmail.com>
2018-02-26 13:40:49 -0500

    Classifier, added Package required,Buggy.

--- Predictive-Maintenance-System-master/Training algorithm.py
+++ Predictive-Maintenance-System-master/Training algorithm.py
@@ -8,6 +8,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.datasets import make_moons, make_circles, make_classification
 from sklearn.neural_network import MLPClassifier
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import NearestNeighbors
 from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.gaussian_process.kernels import RBF
 from sklearn.tree import DecisionTreeClassifier
@@ -19,18 +20,28 @@ from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis;
 # need fields for clasifcation algorithm
 
 #test array 1 represent the training data
-X = np.array([[0], [1], [2], [3],[4],[5],[6]]);
+
+#take CSV not functional
+#T.get_param([CSV])
+
+X = np.array([[0], [1], [2], [3],[4],[5],[6],[8],[9]]);
 #test array 2 represents the tags
-y = np.array([0,0,1,1,0,1,0]);
+y = np.array([1,1,0,0,1,0,0,1,1]);
 #test valdation set
 v= np.array([[2,1],[3,0],[5,1],[6,1],[9,0],[5,1],[4,0]]);
 #classifier
-clf = KNeighborsClassifier(n_neighbors=3);
+clf = KNeighborsClassifier(n_neighbors=9);
 #training classfier with both arrays
 
 #weighted desired score
-dscore =.07;
+dscore =[.7,[9]];
 
+#fits sample data sets to classifier
 clf.fit(X, y);
 
-print(clf.score(X,y,[dscore]));
+#sample prediction
+print(clf.predict([[2]]))
+
+print(clf.predict_proba([[0.5]]))
+#not working scoring
+#print(clf.score(X,y,[dscore]));
