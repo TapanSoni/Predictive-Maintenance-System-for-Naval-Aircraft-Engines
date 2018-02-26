4ba76b3ba5569b1a708be43b297d7a3401b235c0
Nic <ncklasala@gmail.com>
2018-02-26 13:12:34 -0500

    Classifier, added Package required.

--- Predictive-Maintenance-System-master/Training algorithm.py
+++ Predictive-Maintenance-System-master/Training algorithm.py
@@ -13,17 +13,17 @@ from sklearn.gaussian_process.kernels import RBF
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis;
 
 #need to field from the csv file.
 # need fields for clasifcation algorithm
 
 #test array 1 represent the training data
-X = np.array([[0], [1], [2], [3]])
+X = np.array([[0], [1], [2], [3],[4],[5],[6]]);
 #test array 2 represents the tags
-y = np.array([0,0,1,1])
+y = np.array([0,0,1,1,0,1,0]);
 #test valdation set
-v= np.array([[2,1],[3,0],[5,1],[6,1],[9,0]])
+v= np.array([[2,1],[3,0],[5,1],[6,1],[9,0],[5,1],[4,0]]);
 #classifier
 clf = KNeighborsClassifier(n_neighbors=3);
 #training classfier with both arrays
@@ -31,5 +31,6 @@ clf = KNeighborsClassifier(n_neighbors=3);
 #weighted desired score
 dscore =.07;
 
-clf.fit(X, y)
-clf.score(X,y,[dscore]);
+clf.fit(X, y);
+
+print(clf.score(X,y,[dscore]));
