7a28546b24bb16ee1ac68722af62d5958f65ea50
Michael Matthews <ipodappsrcool@gmail.com>
2018-03-04 11:45:34 -0500

    Generating new data now has its own file

---
+++ Predictive-Maintenance-System-master/GenerateData.py
@@ -0,0 +1,21 @@
+#Generates new data randomly based upon lower bound and maxium bound on actual data set
+#note this algorithm can be imporved in the followin ways
+    #have stages of data that its based upon for the example since the engine is deterioting the frist 10% will look different than the second 10% etc.
+    #possibly do somthing with averages and standard deviation
+
+#Todo: when generating new data feature that columns that were only ints in real data set will only be ints in generated data set
+#Todo: program can run more efficently by
+#    1.) parralizing reading of the dataset
+#    2.) exporting and importing generated random dataset, columns avgs. etc so it doesn't have to be everytime the progarm is run
+
+import numpy as np
+import random
+def generate(min, max):
+    new_data_set = np.zeros((589223, 30))
+
+    for x in range(0,30):
+        row = []
+        for y in range(0,589223):
+            new_data_set[y][x] = random.uniform(min[x], max[x])
+
+    return new_data_set
