ae6021b4b6250f3a51485e771817f897d1ac84d5
Tapan Soni <tapansoni97@gmail.com>
2018-03-10 11:04:17 -0500

    Added 3 buttons for classifier selection
    Added functions that change the color of the button of the
    classifier selected
    
    Added a label which shows which one is selected -- might be
    redundant actually

--- Predictive-Maintenance-System-master/GUI.py
+++ Predictive-Maintenance-System-master/GUI.py
@@ -49,6 +49,27 @@ def browsefile():
     fileNameDisplay.config(text=window.fileName)
 
 
+def clickKN():
+    kn.config(bg="orange")
+    dt.config(bg="#F0F0F0")
+    lsvc.config(bg="#F0F0F0")
+    classifierConsole.config(text="K-Nearest Neighbor SELECTED")
+
+
+def clickDT():
+    kn.config(bg="#F0F0F0")
+    dt.config(bg="orange")
+    lsvc.config(bg="#F0F0F0")
+    classifierConsole.config(text="Decision Tree SELECTED")
+
+
+def clickLSVC():
+    kn.config(bg="#F0F0F0")
+    dt.config(bg="#F0F0F0")
+    lsvc.config(bg="orange")
+    classifierConsole.config(text="Linear SVC SELECTED")
+
+
 # Creating the main window
 window = Tk()
 
@@ -77,9 +98,23 @@ runButton = Button(window, text="Run")
 # Output console
 outputConsole = Label(window, bg="white", width="30")
 
+# Classifier console output
+
+classifierConsole = Label(window, bg="white", width="30")
+
+# KNeighbor Selection Button
+kn = Button(window, text="K-Neighbor", width=10, command=clickKN)
+
+# Decision Tree selection button
+dt = Button(window, text="Decision Tree", width=10, command=clickDT)
+
+# Linear SVC selection button
+lsvc = Button(window, text="Linear SVC", width=10, command=clickLSVC)
+
 # Fake labels for positioning th other widgets correctly
 fake_1 = Label(window)
 fake_2 = Label(window)
+fake_3 = Label(window)
 
 # Configurations of the widgets
 fileNamePrompt.config(font="fixedsys 9")
@@ -87,6 +122,9 @@ window.config(background="#EEF4EB")
 outputConsolePrompt.config(font="fixedsys 9")
 fileNameBrowseButton.config(background="#EDDEA4", font="fixedsys 9")
 runButton.config(background="#79FF6D", font="fixedsys 9")
+kn.config(font="fixedsys 9")
+lsvc.config(font="fixedsys 9")
+dt.config(font="fixedsys 9")
 fake_1.config(background="#EEF4EB")
 fake_2.config(background="#EEF4EB")
 
@@ -95,10 +133,18 @@ fileNamePrompt.grid(row=0, column=0, sticky=NSEW)
 fileNameDisplay.grid(row=0, column=1, sticky=NSEW)
 fileNameBrowseButton.grid(row=0, column=2, sticky=NSEW)
 fake_1.grid(row=1, sticky=NSEW)
-runButton.grid(row=2, column=1, sticky=NSEW)
-fake_2.grid(row=3, sticky=NSEW)
-outputConsolePrompt.grid(row=4, column=1, sticky=NSEW)
-outputConsole.grid(row=5, column=1, sticky=NSEW)
+
+# Classifiers
+kn.grid(row=2, column=1, sticky=NSEW)
+dt.grid(row=3, column=1, stick=NSEW)
+lsvc.grid(row=4, column=1, sticky=NSEW)
+
+fake_3.grid(row=5, sticky=NSEW)
+classifierConsole.grid(row=6, column=1, sticky=NSEW)
+fake_2.grid(row=7, sticky=NSEW)
+runButton.grid(row=8, column=1, sticky=NSEW)
+outputConsolePrompt.grid(row=9, column=1, sticky=NSEW)
+outputConsole.grid(row=10, column=1, sticky=NSEW)
 
 # The widgets will fill with the parent
 window.grid_rowconfigure(0, weight=1)
@@ -107,10 +153,17 @@ window.grid_rowconfigure(2, weight=1)
 window.grid_rowconfigure(3, weight=1)
 window.grid_rowconfigure(4, weight=1)
 window.grid_rowconfigure(5, weight=1)
+window.grid_rowconfigure(6, weight=1)
+window.grid_rowconfigure(7, weight=1)
+window.grid_rowconfigure(8, weight=1)
+window.grid_rowconfigure(9, weight=1)
+window.grid_rowconfigure(10, weight=1)
 
 window.grid_columnconfigure(0, weight=1)
 window.grid_columnconfigure(1, weight=1)
 window.grid_columnconfigure(2, weight=1)
+
+
 
 window.mainloop()
 
