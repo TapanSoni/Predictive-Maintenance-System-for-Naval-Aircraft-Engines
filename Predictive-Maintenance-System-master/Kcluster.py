c40b23beb9c0dc4a8d64d6cae5bd784de132a2df
Nic <ncklasala@gmail.com>
2018-04-04 12:44:40 -0400

    added k cluster file

---
+++ Predictive-Maintenance-System-master/Kcluster.py
@@ -0,0 +1,26 @@
+"""
+-----------------------------------------------------------------------------
+
+Rowan Computer Science Dept Spring 2018 Software Engineering Team Ostriches
+Predictive Maintenance System for ASRC Federal Mission Solutions Engineering
+
+Team Ostriches Members:
+Product Owner:  Craig Wert
+ Scrum Master:  John Stranahan
+    Developer:  Tapan Soni
+    Developer:  Michael Matthews
+    Developer:  Joshua Jackson
+    Developer:  Nicholas La Sala
+
+-----------------------------------------------------------------------------
+
+Description:
+ This is the Kcluster.py file. The K cluster classifier is unsuppervised
+ machine learning that will be able to make predictions on the data set.
+
+
+ Last edit: 4/1/18 @ 2:26 PM by Tapan Soni
+
+
+-----------------------------------------------------------------------------
+"""
