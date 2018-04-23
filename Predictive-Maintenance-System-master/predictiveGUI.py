3377331ef156ebeea68b08a0bbec0c53074d98d0
Michael Matthews <ipodappsrcool@gmail.com>
2018-04-23 12:37:27 -0400

    Prediction gui now works

--- Predictive-Maintenance-System-master/predictiveGUI.py
+++ Predictive-Maintenance-System-master/predictiveGUI.py
@@ -50,9 +50,6 @@ random.seed(1331)
 def bandr():
     start_time = time.time()
 
-    tagPercentage = .93
-    valPercentage = .15
-
     # Take in input
     anotherWindow.fileName = filedialog.askopenfilename(filetypes=(("CSV files", ".csv"), ("All files", "*.*")))
     print(anotherWindow.fileName)
@@ -62,7 +59,6 @@ def bandr():
     #whole_data_set = np.genfromtxt(anotherWindow.fileName, delimiter='\t')
     testData = np.genfromtxt(anotherWindow.fileName, delimiter=',')
 
-    #print("datasize: ", whole_data_set.size//30)
     #testData = np.genfromtxt(anotherWindow.fileName, delimiter=',') #the origanl
     print("Data imported")
 
@@ -70,188 +66,22 @@ def bandr():
     #whole_data_set = max_abs_scaler.fit_transform(whole_data_set)
     # print("Data Nomalized")
 
-#Redo subsampling
-
-    # testingData = []
-    # index = 500000
-    # count = 500000
-    #
-    # while(count<509223):
-    #     #testingData.append(whole_data_set[index])
-    #     whole_data_set = np.delete(whole_data_set, index, 0)
-    #     count += 1
-    #     print(count)
-    #
-    # print(whole_data_set.size//30)
-
     import pickle
-    #outputTest = open("testData_500k_501k.pkl", 'wb') #open output file
-    #outputWhole = open("wholeData_500k_501k.pkl", 'wb')
-    #pickle.dump(testingData, outputTest)
-    #pickle.dump(whole_data_set, outputWhole)
-
-    #pkl_file_test = open('testData.pkl', 'rb') # open input file
-    pkl_file_whole = open('wholeData.pkl', 'rb') # open input file
-
-    #testData = pickle.load(pkl_file_test)
-    whole_data_set = pickle.load(pkl_file_whole)
-
-    #outputTest.close() #close output file
-    #outputWhole.close()
-    #pkl_file_test.close() # close input file
-
-    import GenerateTags as tag
-
-    #Generates an array filled with 0's & 1's
-    #tags = tag.generate(tagPercentage, whole_data_set.size // 30)
-    # print("Generated Tags")
-
-    # Splits the data into 2 sections training and validation
-    # Note that this is done for both tags and the actual data
-    import SplitData as split
-
-    #split.split(whole_data_set, tags, valPercentage)
-
-    # trainingData = split.getTrainingData()
-    # trainingTags = split.getTrainingTags()
-    # validationData = split.getValidationData()
-    # validationTags = split.getValidationTags()
-
-    # =============================================================================================================
-    #print("Split DATA")
-    # =============================================================================================================
-
-    #Finds the minimum and maximum of each feature for the training set
-    import FindMinAndMax as find
-
-    # findingMinAndMax = find.FindMinAndMax(trainingData)
-    #
-    # minOf = findingMinAndMax.getMin()
-    # maxOf = findingMinAndMax.getMax()
-    #range = findingMinAndMax.getRan()
-
-    # for index in range(0,30):
-    #     print("Range for row ", index, ": ", maxOf[index]-minOf[index])
-
-    #Generates new random data within the bounds of each feature
-    import GenerateData as gen
-    # new_data_set = gen.generate(minOf, maxOf, len(trainingData))
-    # print("Generated New Data")
-
-    #Generates new tags to go along with the new data
-    # newTags = tag.generate(tagPercentage, new_data_set.size // 30)
-
-    # combines the old and new dataset in that order
-    # Todo: maybe have it so that it randomly combines the dataset sets whilst maintaining sequential order
-    # trainingData = np.concatenate((trainingData, new_data_set), axis=0)
-    # trainingTags = np.concatenate((trainingTags, newTags), axis=0)
-
-#Kcluster
 
     timeForClassifier = time.time() #start timer for the classifier
-    #
-    # from sklearn.cluster import KMeans
-    #
-    # data = np.zeros(shape=(whole_data_set.size // 30, 2))
-    #
-    # for row in range(0, whole_data_set.size // 30):
-    #     firstAverage = 0
-    #     secondAverage = 0
-    #     for column in range(0, 15):
-    #         firstAverage += whole_data_set[row][column]
-    #     for column in range(15, 30):
-    #         secondAverage += whole_data_set[row][column]
-    #     data[row][0] = firstAverage / 2
-    #     data[row][1] = secondAverage / 2
-    #
-    #whole_data_set = data
-    # clusterCenters = [[0, 0]]
-    # newTest = np.zeros(shape=(len(testData), 2))
-    #
-    # for row in range(0, len(testData)):
-    #     firstAverage = 0
-    #     secondAverage = 0
-    #     for column in range(0, 15):
-    #         firstAverage += testData[row][column]
-    #     for column in range(15, 30):
-    #         secondAverage += testData[row][column]
-    #     newTest[row][0] = firstAverage / 2
-    #     newTest[row][1] = secondAverage / 2
-    #
-    # index = whole_data_set.size // 2
-    #
-    # newTest = np.concatenate((whole_data_set, newTest), axis=0)
-    #
+
     average = 0
-    #
-    # while index < len(newTest):
-    #     ok = KMeans(n_clusters=1, random_state=0).fit(newTest[:index])
-    #     if (ok.cluster_centers_[0][0] + ok.cluster_centers_[0][1] > 4.514):
-    #         average = 1
-    #         break
-    #     clusterCenters.append(ok.cluster_centers_[0])
-    #     index += 100
-    #
-    # clusterCenters.pop(0)
-    #
-    # import matplotlib.pyplot as plt
-    #
-    # # pkl_file_whole = open('clusterCenters.pkl', 'rb') # open input file
-    # # clusterCenters = pickle.load(pkl_file_whole)
-    # #
-    # # pkl_file_whole.close()
-    #
-    # sums = np.zeros(shape=(2, len(clusterCenters)))
-    # averages = np.zeros(shape=(2, len(clusterCenters)))
-    #
-    # for index in range(0, len(clusterCenters)):
-    #     averages[0][index] = index
-    #     averages[1][index] = (clusterCenters[index][0] + clusterCenters[index][1]) / 2
-    #     sums[0][index] = index
-    #     sums[1][index] = clusterCenters[index][0] + clusterCenters[index][1]
 
-    # plt.plot(averages[0],averages[1])
-    # plt.show()
+    pkl_file = open('classy.pkl', 'rb') #open input file
+    classy = pickle.load(pkl_file) #unpickle pickled file
 
-    pkl_file_whole.close()
 
-    import KNeighbor as kneighbor
+    for index in range(0, len(testData)):
+        average += classy.predict([testData[index]])
 
-    #kneighbor.classify(299,trainingData,trainingTags,validationData,validationTags)
+    average = average / len(testData)
 
-    #pkl_file = open('classy.pkl', 'rb') #open input file
-
-    #classy = pickle.load(pkl_file) #unpickle pickled file
-
-    #testData = np.asarray(testData)
-
-    #np.savetxt('testData.csv', testData, delimiter=',')
-
-    #np.savetxt('wholedataset.csv', whole_data_set_pickle, delimiter=',')
-
-    indices = []
-    #indices = np.asarray(indices)
-    count = 50
-
-    # for index in range(0, len(testData)):
-    #     average += classy.predict([testData[index]]).item(0)
-    #     if count <= len(testData):
-    #         indices.append(testData[count])
-    #         count += 100
-    #
-    # import plotly.plotly as py
-    # import plotly.graph_objs as go
-    #
-    # trace = go.Scatter(
-    #     x = indices,
-    #     y = testData
-    # )
-    #
-    # data = [trace]
-    #
-    # py.plot(data, filename='testingResults')
-    #
-    #print("Average: ", average/len(testData))
+    print("Average: ", average)
 
     timeForClassifier = time.time() - timeForClassifier #end timer for classifier
 
@@ -264,16 +94,6 @@ def bandr():
     print("********* %s seconds *********" % totaltimeTaken)
     timestampC.config(text="Classifier Run Time: %s seconds" % timeForClassifier)
     timestamp.config(text="  Total Time Elapsed: %s seconds" % totaltimeTaken)
-
-    # Pickling the classifier
-    # fileToStore = "testfile"
-    #
-    # fileObject = open(fileToStore, 'wb')
-    #
-    # pickle.dump(kneighbor.classify(1, trainingData, trainingTags, validationData, validationTags, new_data_set), fileObject)
-    #
-    # fileObject.close()
-
 
 def about():
     aboutWindow = Tk()
@@ -359,7 +179,6 @@ def callback(event):
 def callback2(event):
     webbrowser.open_new("https://github.com/TapanSoni/SoftwareEngineeringFinalProject")
 
-
 anotherWindow = Tk()
 
 anotherWindow.iconbitmap(r'RowanLogo.ico')
@@ -384,7 +203,6 @@ timestampC = Label(anotherWindow, bg = "#F0F0F0", width="30")
 
 runButton = Button(anotherWindow, text="Browse & Run", command=bandr)
 aboutButton = Button(anotherWindow, text = "About", command=about)
-
 
 # Configurations of the widgets
 fileNamePrompt.config(font="fixedsys 9")
